ead41fbcc87e246da4e4c896267db0646e48ce15
samliu1234 <64169932+samliu1234@users.noreply.github.com>
2020-06-16 21:29:45 -0400

    beginning of work

---
+++ St Augustine CHS/tasksController.swift
@@ -0,0 +1,20 @@
+//
+//  tasksController.swift
+//  St Augustine CHS
+//
+//  Created by Sam Liu on 2020-06-16.
+//  Copyright © 2020 St Augustine CHS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class tasksController: UITableViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var noteLabel: UILabel!
+    @IBOutlet weak var dueDateLabel: UILabel!
+    @IBOutlet weak var doneLabel: UILabel!
+    
+}
+

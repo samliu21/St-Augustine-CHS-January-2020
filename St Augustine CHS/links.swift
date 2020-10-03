17777918b3d0cdc16564234d7018dc0c63d8458c
matthewm588 <64167590+matthewm588@users.noreply.github.com>
2020-10-03 19:11:54 -0400

    added titan times link

--- St Augustine CHS/links.swift
+++ St Augustine CHS/links.swift
@@ -14,7 +14,8 @@ class links: UIViewController {
     @IBOutlet weak var prayerRequestButton: UIButton!
     @IBOutlet weak var background2: UILabel!
     @IBOutlet weak var twitterButton: UIButton!
-    
+    @IBOutlet weak var background3: UILabel!
+    @IBOutlet weak var titanTimesButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +23,24 @@ class links: UIViewController {
         self.prayerRequestButton.layer.cornerRadius = 20
         self.background2.layer.cornerRadius = 20
         self.twitterButton.layer.cornerRadius = 20
+        self.background3.layer.cornerRadius = 20
+        self.titanTimesButton.layer.cornerRadius = 20
+        self.titanTimesButton.layer.zPosition = 1
     }
     
     @IBAction func didTapPrayer(_ sender: Any) {
         UIApplication.shared.open(NSURL(string: "https://forms.gle/gVqcL4UPv3uX7hVz6")! as URL)
     }
-
+    
+    
     @IBAction func didTapTwitter(_ sender: Any) {
         UIApplication.shared.open(NSURL(string:"https://twitter.com/STAUpdates")! as URL)
     }
     
+    @IBAction func didTapTitanTimes(_ sender: Any) {
+        UIApplication.shared.open(NSURL(string:"https://titantimes.live/")! as URL)
+    }
+    
 }
+
+
